4e0556aad3b2f3fd7fb35c3d03b35194cb84c495
PeteBoyRocket <peteboyrocket@hotmail.com>
2020-04-07 21:09:22 +0100

    Added a move on click guy

---
+++ TomFrancis/Assets/MoveToClickPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        this.agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                this.agent.destination = hit.point;
+            }
+        }
+    }
+}
